cc271fe830b3d521d01fd221cc2530661354edc7
Jose Moises <jmvilla2301@gmail.com>
2024-06-17 16:56:11 -0500

    UC3_Parte02_VILLANUEVA

--- UC3_Bravo_Pena_Torres_Villanueva/miapp/views.py
+++ UC3_Bravo_Pena_Torres_Villanueva/miapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def inicio(request):
+    return render(request, 'inicio.html')
