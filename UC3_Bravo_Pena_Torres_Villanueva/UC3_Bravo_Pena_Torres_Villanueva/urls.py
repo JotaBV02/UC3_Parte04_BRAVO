cf138acd79501ef0bb03db3bfd24777cd78295cd
Carlos P <2123110286@untels.edu.pe>
2024-06-17 17:28:30 -0500

    UC3_PARTE03_PENA

--- UC3_Bravo_Pena_Torres_Villanueva/UC3_Bravo_Pena_Torres_Villanueva/urls.py
+++ UC3_Bravo_Pena_Torres_Villanueva/UC3_Bravo_Pena_Torres_Villanueva/urls.py
@@ -22,4 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = "index"),
     path('inicio/', views.inicio, name = "inicio"),
+    path('primos/',views.primos,name="primos"),
+    path('primos/<int:a>',views.primos,name="primos"),
+    path('primos/<int:a>/<int:b>',views.primos,name="primos")
 ]
--- UC3_Bravo_Pena_Torres_Villanueva/miapp/views.py
+++ UC3_Bravo_Pena_Torres_Villanueva/miapp/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
-
+layout ="""
+<h1> Lenguaje de Programacion III - 2024) | UC3 </h1>
+<hr/>
+<h4>Menu:</h4>
+<ul>
+    <li>
+        <a href="/inicio"> Inicio</a>
+        </li>
+        <li><a href="/primos">Numeros primos</a></li>
+    </ul>
+<hr/>
+"""
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -8,3 +19,28 @@ def index(request):
 
 def inicio(request):
     return render(request, 'inicio.html')
+
+def es_primo(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def primos(request, a=0, b=100):
+    if a > b:
+        return redirect('primos', a=b, b=a)
+    
+    resultado = f"""
+        <h2> Números primos de [{a},{b}] </h2>
+        Resultado: <br>
+        <ul> 
+    """
+    
+    for num in range(a, b + 1):
+        if es_primo(num):
+            resultado += f"<li> {num} </li>"
+    
+    resultado += "</ul>"
+    return HttpResponse(layout + resultado)
